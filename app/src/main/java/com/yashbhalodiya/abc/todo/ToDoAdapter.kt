43d17f4d6cd819d676a7fa6f681f3cb937c7641a
YashBhalodiya <yashbhalodiya650@gmail.com>
2024-07-03 17:04:19 +0530

    solved small issue

--- app/src/main/java/com/yashbhalodiya/abc/todo/ToDoAdapter.kt
+++ app/src/main/java/com/yashbhalodiya/abc/todo/ToDoAdapter.kt
@@ -7,14 +7,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.yashbhalodiya.abc.databinding.TodoItemBinding
 import java.util.ArrayList
 
-class ToDoAdapter(var context : Context,
-                  var datalist : ArrayList<ToDoModel>,
-                  var onDelete : (Int) -> Unit) //Callback fun to delete
+class ToDoAdapter(
+    var context: Context,
+    var datalist: ArrayList<ToDoModel>,
+    var onDelete: (Int) -> Unit
+) //Callback fun to delete
     : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
-    inner class ToDoViewHolder(var binding : TodoItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ToDoViewHolder(var binding: TodoItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        val binding = TodoItemBinding.inflate(LayoutInflater.from(context))
+        val binding = TodoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ToDoViewHolder(binding)
     }
 
@@ -23,10 +25,13 @@ class ToDoAdapter(var context : Context,
     }
 
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
-        holder.binding.todoTitleTxt.text = datalist.get(position).title
-        holder.binding.todoDescTxt.text = datalist.get(position).description
-        holder.binding.deleteBtn.setOnClickListener {
-            onDelete(position)
+
+        if (position < datalist.size) {
+            holder.binding.todoTitleTxt.text = datalist.get(position).title
+            holder.binding.todoDescTxt.text = datalist.get(position).description
+            holder.binding.deleteBtn.setOnClickListener {
+                onDelete(position)
+            }
         }
     }
 }
